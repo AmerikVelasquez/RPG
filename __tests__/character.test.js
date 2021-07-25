27d375a83d27eeed289d6ac46db234e257bac2c9
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-25 12:07:44 -0700

    add good fail to character class test
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ __tests__/character.test.js
@@ -0,0 +1,18 @@
+import Character from './../src/character.js'
+
+describe('Character', () =>{
+
+  test('should correctly create a character with appropriate properites based on the argument', () => {
+    const character1 = new Character("Martial Artist");
+    expect(character1.health).toEqual(5);
+    expect(character1.defence).toEqual(5);
+    expect(character1.attack).toEqual(5);
+    expect(character1.magicAttack).toEqual(0);
+    expect(character1.mana).toEqual(0);
+    expect(character1.intelligence).toEqual(5);
+    expect(character1.manaReplenishmentPotion).toEqual(3);
+    expect(character1.attackBoostPotion).toEqual(3);
+    expect(character1.healthBoostPotion).toEqual(3);
+    expect(character1.level).toEqual(1);
+  });
+});
