8570cc41a2cb54aff184b72ab885a4cae797eea8
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-26 19:40:43 -0700

    add dragon object and good fail test associate with it
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ __tests__/dragon.test.js
@@ -0,0 +1,11 @@
+import Dragon from './../src/dragon.js'
+
+describe('Dragon', () => {
+
+  test('should instantiate a dragon object', () => {
+    let dragon = new Dragon();
+    expect(dragon.attack).toEqual(5);
+    expect(dragon.health).toEqual(10);
+    expect(dragon.defense).toEqual(4);
+  });
+});
